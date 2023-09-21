8b9769c8bd6a8cec8d2d2321e8b23fde687f85a7
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-09-21 18:19:21 +0700

    workspace: Configure EditorConfig and settings

--- Deta.Net/Class1.cs
+++ Deta.Net/Class1.cs
@@ -1,4 +1,7 @@
-﻿namespace Deta.Net;
+﻿// Copyright (c) shigeru22. Licensed under the MIT license.
+// See LICENSE in the repository root for details.
+
+namespace Deta.Net;
 
 public class Class1
 {
